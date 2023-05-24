0a5007d6f7879a987f2b7a54ea3073e220daef66
Andrew Pham <andrew.pham@shopify.com>
2023-05-24 17:27:24 -0400

    Implemented pagination on the home page

--- models/pet.js
+++ models/pet.js
@@ -1,19 +1,30 @@
-"use strict";
+'use strict';
+
+const { mongo } = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+mongoosePaginate.paginate.options = {
+  limit: 3,
+};
 
 const mongoose = require('mongoose'),
-        Schema = mongoose.Schema;
+  Schema = mongoose.Schema;
 
-const PetSchema = new Schema({
-    name            : { type: String, required: true }
-  , species         : { type: String }
-  , birthday        : { type: Date }
-  , picUrl          : { type: String }
-  , picUrlSq        : { type: String }
-  , favoriteFood    : { type: String }
-  , description     : { type: String }
-},
-{
-  timestamps: true
-});
+const PetSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    species: { type: String },
+    birthday: { type: Date },
+    picUrl: { type: String },
+    picUrlSq: { type: String },
+    favoriteFood: { type: String },
+    description: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+PetSchema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model('Pet', PetSchema);
